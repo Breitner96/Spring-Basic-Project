996a501305140bdd83a9dda2aa783f4412b668dd
Breitner96 <jeanbreitnermora@gmail.com>
2021-11-06 14:45:43 -0500

    First Commit

---
+++ src/main/java/com/platzi/projectoplatzi/ProjectoPlatziApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.projectoplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoPlatziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectoPlatziApplication.class, args);
+    }
+
+}
